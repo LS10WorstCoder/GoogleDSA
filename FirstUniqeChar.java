dffc36cd68d2348704785533036e236d363bd2b0
LS10WorstCoder <ofc.tedsold@gmail.com>
2024-06-16 23:29:04 -0400

    FirstUniqueDone

--- FirstUniqeChar.java
+++ FirstUniqeChar.java
@@ -1,15 +1,19 @@
+import java.util.*;
 class Solution {
     public int firstUniqChar(String s) {
-        Map<Character,ArrayList<Integer>> multiMap = new HashMap<>();
+        Map<Character,Integer> hashMap = new HashMap<>();
         int l = s.length();
         for(int i = 0; i < l; i++){
             char n = s.charAt(i);
-            if(!multiMap.containsKey(n)){
-                multiMap.put(n,i);
-            }
-        }
-        for(){
+            hashMap.put(n,hashMap.getOrDefault(n, 0) + 1);
             
         }
+        for(int i = 0; i < l; i++){
+            char n = s.charAt(i);
+            if(hashMap.get(n) == 1){
+                return i;
+            }
+        }
+        return -1;
     }
 }
